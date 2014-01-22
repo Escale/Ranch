e075a28276c76b92f8c0e53cf8b49c6f0827daef
Flora-L <flora@MacBook-Pro-de-Flora.local>
2014-01-22 16:25:03 +0100

    Create jquery component checkAll

--- js/checkall.js
+++ js/checkall.js
@@ -1,22 +1,70 @@
-		function checkAll(group){
-			var checkboxes = document.querySelectorAll("input[type='checkbox'][data-multicheck='"+ group +"']");
-			for(var i=0; i<checkboxes.length; i++) {
-				checkboxes[i].checked="checked";
-			}
-		}
-		
-		var allcheck = document.querySelectorAll("input[data-multicheck-toggle]");
-		for(var i=0; i<allcheck.length; i++){
-			var firstCheck = allcheck[i];
-			if (firstCheck.addEventListener){ //Première condition : pour +IE 8 
-			  	firstCheck.addEventListener('click', function(e) {
-			  		var colour = e.target.getAttribute('data-multicheck-toggle');
-			  		checkAll(colour);
-			  		console.log(colour);
-			  	}
-			  	, false);
-			} 
-			else if (firstCheck.attachEvent) {
-			  firstCheck.attachEvent('click', function () {checkAll(group)});
-			}
-		}
+/* ========================================================================
+ * Ranch: checkall.js vX.X.X @TODO
+ * URL DEMO @TODO
+ * ========================================================================
+ * Copyright 2014 Escale Digitale SARL.
+ * Licensed under MIT (URL LICENCE @TODO)
+ * ======================================================================== */
+
+
++function ($) {
+	'use strict';
+
+	// CHECKALL CLASS DEFINITION
+	// ====================
+
+	var CheckAll = function (element) {
+		this.element = $(element);
+		this.collection = this.element.attr("data-multicheck-toggle");
+	}
+
+
+	CheckAll.prototype.toggle = function() {
+		$('input[type="checkbox"][data-multicheck="'+ this.collection +'"]').prop('checked',this.element.prop('checked'));
+	}
+
+	CheckAll.prototype.checkAll = function() {
+		$('input[type="checkbox"][data-multicheck="'+ this.collection +'"]').prop('checked',true);
+	}
+
+	CheckAll.prototype.unCheckAll = function() {
+		$('input[type="checkbox"][data-multicheck="'+ this.collection +'"]').prop('checked',false);
+	}
+
+
+	// CHECKALL PLUGIN DEFINITION
+	// =====================
+
+	var old = $.fn.checkAll
+
+	$.fn.checkAll = function ( option ) {
+		return this.each(function () {
+			var $this = $(this)
+			var data  = $this.data('bs.checkAll')
+
+			if (!data) $this.data('bs.checkAll', (data = new CheckAll(this)))
+			if (typeof option == 'string') data[option]()
+		})
+	}
+
+	$.fn.checkAll.Constructor = CheckAll
+
+
+	// CHECKALL NO CONFLICT
+	// ===============
+
+	$.fn.checkAll.noConflict = function () {
+		$.fn.checkAll = old
+		return this
+	}
+
+
+	// CHECKALL DATA-API
+	// ============
+
+	$("input[data-multicheck-toggle]").change(function() {
+		$(this).checkAll('toggle');
+	});
+
+
+}(jQuery);
